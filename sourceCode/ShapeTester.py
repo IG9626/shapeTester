cff4b5d0ba7322565969a5284374535f4f29b023
IG9626 <31745226+IG9626@users.noreply.github.com>
2018-02-20 14:13:45 -0700

    Add files via upload

---
+++ sourceCode/ShapeTester.py
@@ -0,0 +1,17 @@
+#import Box, Sphere, Pyramid
+print("welcome...with this program you can calculate the surface area of diffrent shapes")
+print("if you want to calculate...")
+print("a box type b")
+print("a sphere type s")
+print("a pyramid type p")
+test = input("what shape do you want to calculate?:")
+if test == "b":
+    print("box")
+elif test == "s":
+    print("sphere")
+elif test == "p":
+    print("pyramid")
+else:
+    print("error-not an option")
+    
+
